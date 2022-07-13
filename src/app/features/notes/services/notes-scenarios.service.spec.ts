d6f1bce690901cc9c060f446cd9899b9a777cac1
Sampjam <sampjam@gmail.com>
2022-07-13 10:44:08 -0400

    comment out notes-scenarios service

--- src/app/features/notes/services/notes-scenarios.service.spec.ts
+++ src/app/features/notes/services/notes-scenarios.service.spec.ts
@@ -1,30 +1,27 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { inject, TestBed } from '@angular/core/testing';
-import { map, pipe, tap } from 'rxjs';
+// import { HttpClientModule } from '@angular/common/http';
+// import { inject, TestBed } from '@angular/core/testing';
+// import { map, pipe, tap } from 'rxjs';
 
-import {
-  HttpClientTestingModule,
-  HttpTestingController,
-} from '@angular/common/http/testing';
+// import { HttpTestingController } from '@angular/common/http/testing';
 
-import { NotesScenariosService } from './notes-scenarios.service';
+// import { NotesScenariosService } from './notes-scenarios.service';
 
-describe('NotesScenariosService', () => {
-  let service: NotesScenariosService;
-  let httpTestingController: HttpTestingController;
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientModule],
-      providers: [NotesScenariosService],
-    });
-    service = TestBed.inject(NotesScenariosService);
-  });
+// describe('NotesScenariosService', () => {
+//   let service: NotesScenariosService;
+//   let httpTestingController: HttpTestingController;
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       imports: [HttpClientModule],
+//       providers: [NotesScenariosService],
+//     });
+//     service = TestBed.inject(NotesScenariosService);
+//   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+//   it('should be created', () => {
+//     expect(service).toBeTruthy();
+//   });
 
-  it('should get scenarios', () => {
-    service.scenario$.subscribe((d) => expect(d?.date).toBe('5/12/2022'));
-  });
-});
+//   it('should get scenarios', () => {
+//     service.scenario$.subscribe((d) => expect(d?.date).toBe('5/12/2022'));
+//   });
+// });
--- src/app/features/notes/services/notes-scenarios.service.ts
+++ src/app/features/notes/services/notes-scenarios.service.ts
@@ -1,32 +1,32 @@
-import { Inject, Injectable, LOCALE_ID } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { NotesScenarios } from '../notes-scenario';
-import { filter, map, take, tap } from 'rxjs';
-import { formatDate } from '@angular/common';
+// import { Inject, Injectable, LOCALE_ID } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { NotesScenarios } from '../notes-scenario';
+// import { filter, map, take, tap } from 'rxjs';
+// import { formatDate } from '@angular/common';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class NotesScenariosService {
-  constructor(
-    private http: HttpClient,
-    @Inject(LOCALE_ID) private locale: string
-  ) {}
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class NotesScenariosService {
+//   constructor(
+//     private http: HttpClient,
+//     @Inject(LOCALE_ID) private locale: string
+//   ) {}
 
-  private scenariosPath = 'assets/data/notes-scenarios.json';
-  scenario$ = this.http.get<NotesScenarios[]>(this.scenariosPath).pipe(
-    map((scenarios) =>
-      scenarios.find(
-        (s) => s.date === formatDate(Date.now(), 'M/dd/YYYY', this.locale)
-      )
-    ),
-    tap((s) =>
-      console.log(
-        `got the scenario! date: ${s?.date} scenarios: ${s?.scenarios}`
-      )
-    )
-  );
-  // $scenario = this.http
-  //   .get<NotesScenarios[]>(this.scenariosPath)
-  //   .pipe(tap((s) => ('scenarios: ' + s.forEach(s => console.log(`date ${s.date} scenarios ${s.scenarios}`)))));
-}
+//   private scenariosPath = 'assets/data/notes-scenarios.json';
+//   scenario$ = this.http.get<NotesScenarios[]>(this.scenariosPath).pipe(
+//     map((scenarios) =>
+//       scenarios.find(
+//         (s) => s.date === formatDate(Date.now(), 'M/dd/YYYY', this.locale)
+//       )
+//     ),
+//     tap((s) =>
+//       console.log(
+//         `got the scenario! date: ${s?.date} scenarios: ${s?.scenarios}`
+//       )
+//     )
+//   );
+//   // $scenario = this.http
+//   //   .get<NotesScenarios[]>(this.scenariosPath)
+//   //   .pipe(tap((s) => ('scenarios: ' + s.forEach(s => console.log(`date ${s.date} scenarios ${s.scenarios}`)))));
+// }
